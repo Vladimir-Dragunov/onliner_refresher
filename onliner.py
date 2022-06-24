e04cb4160159f3e2a597fd52215798fde96e1721
Vladimir Marushkevich <92493007+dragunov-m@users.noreply.github.com>
2022-06-24 16:08:23 +0300

    big update onliner refresher

--- onliner.py
+++ onliner.py
@@ -1,47 +1,89 @@
-import os
 import time
-
-from pyvirtualdisplay import Display
+from datetime import datetime
+from config import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 
-chrome_options = Options()
+# set date and time in dd/mm/YY H/M/S
+now = datetime.now()
+date_cron = now.strftime("%d/%m/%Y %H:%M:%S")
+#
 
-display = Display(visible=0, size=(800, 600))
-display.start()
+# chrome options
+driver_options = webdriver.ChromeOptions()
+driver_options.add_argument("no-sandbox")
+driver_options.add_argument("--headless")
+driver_options.add_argument("--disable-gpu")
+driver_options.add_argument("--window-size=1920,1080")
+driver_options.add_argument("--disable-extensions")
+driver_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36")
+#
 
-Login = os.environ['input_login_onliner'] # Ваш логин
-Password = os.environ['input_password'] # Ваш пароль
+# login and password from config.py
+Login = input_login_hh
+Password = input_password
+#
 
-crontab_chrome_path = '/usr/bin/chromedriver'
-crontab_chrome = '/home/dragunov/Logs/crontab_chrome_onliner.log' # Путь для лога
-service = Service(executable_path=crontab_chrome_path, log_path=crontab_chrome) # Запуск бинарника
-driver = webdriver.Chrome(options=chrome_options, service=service)
+# setting path for web driver path and log path
+chrome_service = Service(executable_path=crontab_chromedriver_path)
+driver = webdriver.Chrome(service=chrome_service, service_log_path=crontab_chromedriver_log, options=driver_options)
+#
 
-driver.get("https://profile.onliner.by/")
-driver.set_page_load_timeout(7)
+url = ["https://profile.onliner.by/", "https://baraholka.onliner.by/search.php?type=ufleamarket"]
 
-input_login = driver.find_element(By.CSS_SELECTOR, "input.auth-input[type='text']") # Внесение данных в поле логин
-input_login.send_keys(Login)
-time.sleep(5)
+try:
+    driver.get(url[0])
+    driver.set_page_load_timeout(7)
+except TimeoutException:
+    print(date_cron, "Can't go on", url[0])
+    driver.quit()
 
-input_password = driver.find_element(By.CSS_SELECTOR, "input.auth-input[type='password']")  # Внесение данных в поле пароль
-input_password.send_keys(Password)
-time.sleep(5)
+try:
+    input_login = driver.find_element(By.CSS_SELECTOR, "input.auth-input[type='text']")  # Внесение данных в поле логин
+    input_login.send_keys(Login)
+    time.sleep(5)
+except NoSuchElementException:
+    print(date_cron, "Can't see login input")
+    driver.quit()
 
-button_submit = driver.find_element(By.CSS_SELECTOR, "button.auth-button[type='submit']") # Вход на сайт
-button_submit.click()
-time.sleep(5)
+try:
+    input_password = driver.find_element(By.CSS_SELECTOR, "input.auth-input[type='password']")  # Внесение данных в поле пароль
+    input_password.send_keys(Password)
+    time.sleep(5)
+except NoSuchElementException:
+    print(date_cron, "Can't see password input")
+    driver.quit()
 
-driver.get("https://baraholka.onliner.by/search.php?type=ufleamarket")
-driver.set_page_load_timeout(10)
+try:
+    button_submit = driver.find_element(By.CSS_SELECTOR, "button.auth-button[type='submit']")  # Вход на сайт
+    button_submit.click()
+    time.sleep(5)
+except NoSuchElementException:
+    print(date_cron, "Can't click on submit button")
+    driver.quit()
 
-checkbox = driver.find_element(By.ID, "select-all-my-adverts")
-checkbox.click()
-time.sleep(5)
+try:
+    driver.get(url[1])
+    driver.set_page_load_timeout(10)
+except TimeoutException:
+    print(date_cron, "Can't go on", url[1])
+    driver.quit()
 
-up_button = driver.find_element(By.CSS_SELECTOR, "a.btn-up-2-orange")
-up_button.click()
-driver.close()
+try:
+    checkbox = driver.find_element(By.ID, "select-all-my-adverts")
+    checkbox.click()
+    time.sleep(5)
+except NoSuchElementException:
+    print(date_cron, "Can't click on select all button")
+    driver.quit()
+
+try:
+    up_button = driver.find_element(By.CSS_SELECTOR, "a.btn-up-2-orange")
+    up_button.click()
+except NoSuchElementException:
+    print(date_cron, "Can't click on up button")
+    driver.quit()
+    
